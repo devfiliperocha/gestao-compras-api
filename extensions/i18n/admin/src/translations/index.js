d469a9167a0d916145ec78aafb686b5b9b5e744f
Emanuel Filipe <emanuel8926@gmail.com>
2021-09-17 15:09:06 -0300

    docs(translation): added all plugins and admin translations for pt-BR

---
+++ extensions/i18n/admin/src/translations/index.js
@@ -0,0 +1,13 @@
+import en from './en.json';
+import fr from './fr.json';
+import zhHans from './zh-Hans.json';
+import ptBr from './pt-BR.json';
+
+const trads = {
+  en,
+  fr,
+  'zh-Hans': zhHans,
+  'pt-BR': ptBr,
+};
+
+export default trads;
