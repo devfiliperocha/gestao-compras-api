828f1465c39ecc25d627c06a7ad541073a3de0e9
Emanuel Filipe <emanuel8926@gmail.com>
2021-09-17 15:16:10 -0300

    chore: customize app info page

--- admin/admin.config.js
+++ admin/admin.config.js
@@ -5,6 +5,7 @@ module.exports = {
       new webpack.DefinePlugin({
         CUSTOM_VARIABLES: {
           ORGAN_NAME: JSON.stringify(process.env.ORGAN_NAME),
+          DATABASE_CLIENT: JSON.stringify(process.env.DATABASE_CLIENT),
         },
       }),
     );
